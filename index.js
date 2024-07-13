490e3b457f2ddc259896346cfa2a6dedbf4ece1c
Narender Singh <109752675+NarenderSD@users.noreply.github.com>
2024-07-13 15:14:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,16 @@ const port = process.env.PORT || 4000;
 
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send( );
+});
+
+app.get('/api/v1/message/getall', (req, res) => {
+  res.send( );
+});
+
+
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost${port}`);
 });
